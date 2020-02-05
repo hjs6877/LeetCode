f90a54a6f00da28f4c50107cfcae6ac03b5deda7
hjs6877 <hjs6877@gmail.com>
2020-02-05 17:45:45 +0900

    Create ReverseInteger before refactoring

---
+++ src/com/leetcode/easy/ReverseInteger.java
@@ -0,0 +1,29 @@
+package com.leetcode.easy;
+
+public class ReverseInteger {
+    public static void main(String[] args){
+        int num = 4423445;
+        int result = reverse(num);
+
+        System.out.println(result);
+    }
+
+    public static int reverse(int x) {
+        String numAsString = String.valueOf(Math.abs(Long.valueOf(x)));
+        String[] nums = numAsString.split("");
+        String[] rNums = new String[numAsString.length()];
+        for(int i= 0; i < nums.length; i++){
+
+            rNums[(nums.length - i) - 1] = nums[i];
+        }
+
+        long result = Long.parseLong(String.join("", rNums));
+
+        if(x < 0)
+            result = -result;
+        if(result < Math.round(Math.pow(-2, 31)) || result > Math.round(Math.pow(2, 31)) - 1)
+            result = 0;
+
+        return (int)result;
+    }
+}
